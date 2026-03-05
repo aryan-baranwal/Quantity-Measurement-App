1f1a4f6ef766b0e54406682c2562d578aedcdf72
Aryan Baranwal <aryanbaranwal2004@gmail.com>
2026-03-05 16:43:20 +0530

    [Aryan Baranwal] feat(UC1): implement feet measurement equality with equals() override

---
+++ src/main/java/com/bridgelabz/QuantityMeasurementApp.java
@@ -0,0 +1,42 @@
+package com.bridgelabz;
+
+public class QuantityMeasurementApp {
+
+    // Inner class to represent Feet measurement
+    public static class Feet {
+
+        private final double value;
+
+        public Feet(double value) {
+            this.value = value;
+        }
+
+        @Override
+        public boolean equals(Object obj) {
+
+            // same reference
+            if (this == obj)
+                return true;
+
+            // null check
+            if (obj == null)
+                return false;
+
+            // type check
+            if (getClass() != obj.getClass())
+                return false;
+
+            Feet other = (Feet) obj;
+
+            return Double.compare(this.value, other.value) == 0;
+        }
+    }
+
+    public static void main(String[] args) {
+
+        Feet f1 = new Feet(1.0);
+        Feet f2 = new Feet(1.0);
+
+        System.out.println("Equal : " + f1.equals(f2));
+    }
+}
